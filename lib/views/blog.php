fffdd32674340cd6fd7800e2399d892f6f73c30d
Severak <strajt9@seznam.cz>
2017-02-01 01:12:34 +0100

    umime prohlizet vsechno

--- lib/views/blog.php
+++ lib/views/blog.php
@@ -20,22 +20,67 @@ foreach ($posts as $post) {
 <div class="pure-g">
 	<div class="pure-u-1-5"><img class="pure-img" src="<?php echo $blog_image; ?>"/><br/><?php echo $blog_name; ?></div>
 	<div class="pure-u-4-5">
-	<?php echo date('j.n.Y', strtotime($post['datetime'])); ?><br/>
+	<?php echo date('j.n.Y H:i:s', strtotime($post['datetime'])); ?><br/>
 	
-	<?php if ($post['type']==1): ?>
-		<?php if (!empty($post['title'])) {
+	<?php 
+	if ($post['type']==1) { // text
+		if (!empty($post['title'])) {
 			echo '<h2>' . $post['title'] . '</h2>';
 		} 
 		echo $post['body'];
-		?>
-	<?php elseif ($post['type']==4): ?>
-		<img class="pure-img" src="<?php echo $post['url']; ?>"/>
-		<?php if (!empty($post['body'])) {
+	} else if ($post['type']==2) { // link	
+		echo '<a href="' . $post['source'] . '">' . $post['title'] . '</a>';
+		if (!empty($post['body'])) {
 			echo '<p>' . $post['body'] . '</p>';
-		} ?>
-	<?php else: ?>	
-		<?php dump($post); ?>
-	<?php endif; ?>
+		}
+	} else if ($post['type']==3) { // link	
+		echo '<blockquote>'. $post['body'] .'<br/> &mdash; ' . $post['title'] . '</blockquote>';
+		if (!empty($post['source'])) {
+			echo '<p>from <a href="' . $post['source'] . '">' . $post['source'] . '</a></p>';
+		}
+	} else if ($post['type']==4) { // image
+		echo '<img class="pure-img" src="' . $post['url'] . '"/>';
+		if (!empty($post['body'])) {
+			echo '<p>' . $post['body'] . '</p>';
+		}
+		if (!empty($post['source'])) {
+			echo '<p>from <a href="' . $post['source'] . '">' . $post['source'] . '</a></p>';
+		}
+	} else if ($post['type']==5) { // video (URL for now)
+		echo '<a href="' . $post['url'] . '">' . $post['body'] . '</a>';
+		if (!empty($post['source'])) {
+			echo '<p>from <a href="' . $post['source'] . '">' . $post['source'] . '</a></p>';
+		}
+	} else if ($post['type']==6) { // file
+		echo '<a href="' . $post['url'] . '">' . $post['title'] . '</a> <small>' . $post['info'] . '</small>';
+		if (!empty($post['body'])) {
+			echo '<p>' . $post['body'] . '</p>';
+		}
+	} else if ($post['type']==7) { // rating
+		echo '<h2>' . $post['title'] . '</h2>';
+		echo 'rating: ' . str_repeat('*', $post['rating']);
+		if (!empty($post['body'])) {
+			echo '<p>' . $post['body'] . '</p>';
+		}
+		// todo ULR of video
+		if (!empty($post['source'])) {
+			echo '<p>from <a href="' . $post['source'] . '">' . $post['source'] . '</a></p>';
+		}
+	} else if ($post['type']==8) { // event
+		if (!empty($post['title'])) {
+			echo '<h2>' . $post['title'] . '</h2>';
+		}
+		if (!empty($post['body'])) {
+			echo '<p>' . $post['body'] . '</p>';
+		}
+		if (!empty($post['url'])) {
+			echo '<img src="' . $post['url'] . '" class="pure-img">';
+		}
+		echo '<p>from: ' . $post['start_date'] . '</p>';
+		echo '<p>to: ' . $post['end_date'] . '</p>';
+		echo '<p>where: ' . $post['location'] . '</p>';
+	} else dump($post);
+	?>
 
 
 		<!-- repost a spol -->
@@ -73,7 +118,7 @@ if ($i==30) {
 }
 
 if (isset($posts[30]['datetime'])) {
-	echo '<a href="/'.$blog_name.'?since='.date('c', strtotime($posts[30]['datetime'])).'">load moar</a>';
+	echo '<a href="/'.$blog_name.'?since='.date('c', strtotime($posts[30]['datetime'])).'">load moar</a><br/><br/><br/>';
 } else {
 	echo 'You have reached teh end...';
 }
