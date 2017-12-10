1ca0e8f82b6d14b6c5836874978a9c67b57ef2fb
Severak <strajt9@seznam.cz>
2017-12-10 17:04:37 +0100

    working friends page

--- lib/routes/blogs.php
+++ lib/routes/blogs.php
@@ -135,4 +135,55 @@ Flight::route('/@name', function($name){
 	Flight::render('footer', []);
 });
 
-// todo: /friends
+// /@blog/friends
+Flight::route('/@name/friends', function($name){
+	$db = Flight::db();
+	
+	$blog = $db->from('blogs')->where('name', $name)->where('is_visible', 1)->select()->one();
+	
+	if (empty($blog)) {
+		Flight::notFound();
+	}
+	
+	$sel = $db
+		->from('posts')
+		->join('blogs', ['blog_id'=>'blogs.id'])
+		->join('friendships', ['friendships.to_blog_id'=>'blogs.id'])
+		->where('friendships.from_blog_id', $blog['id'])
+		->where('posts.is_visible', 1);
+	
+	if (!empty($_GET['since'])) {
+		$sel->where('datetime <= ', strtotime($_GET['since']) );
+	}
+	
+	$sel->limit(31)
+		->sortDesc('datetime')
+		->select('posts.*, blogs.name as name, blogs.avatar_url');
+	
+	$posts = $sel->many();
+
+	$moreLink = null;
+	$theEnd = true;
+
+	if (count($posts)==31) {
+		$lastPost = array_pop($posts);
+		$moreLink = '/' . $blog['name'] . '/friends?since=' . date('Y-m-d\TH:i:s', $lastPost['datetime']);
+		$theEnd = false;
+	}
+
+	Flight::render('header', ['title' => $blog["title"] ]);
+	Flight::render('blog_header', [
+		'title'=> $blog['title'],
+		'about'=> $blog['about'],
+		'avatar_url'=> $blog['avatar_url']
+	]);
+	Flight::render('posts', [
+		'posts'=>$posts,
+		'blog' => $blog,
+		'user' => Flight::user(),
+		'more_link'=>$moreLink,
+		'the_end'=>$theEnd
+	]);
+	Flight::render('footer', []);
+});
+
