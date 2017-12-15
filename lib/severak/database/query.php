9e86f5a170215479c35610b5d68bf07946b74c28
Severak <strajt9@seznam.cz>
2017-12-15 17:12:44 +0100

    interpolation of query

--- lib/severak/database/query.php
+++ lib/severak/database/query.php
@@ -28,12 +28,37 @@ class query
 				$this->sql . ' ' . $other,
 				array_merge($this->params, $params)
 			);
-		} elseif (is_object($other)) {
+		} elseif (is_object($other) && $other instanceof query) {
 			return new query(
 				$this->sql . ' '. $other->sql,
 				array_merge($this->params, $other->params)
 			);
 		}
 		throw new usageException('Bad parameters!');
+	}
+	
+	public function interpolate()
+	{
+		$pdo = new \PDO('sqlite::memory:');
+		$sql = $this->sql;
+		foreach ($this->params as $param) {
+			if (is_null($param)) {
+				$param = 'NULL';
+			} elseif (is_numeric($param)) {
+				$param = sprintf('%d', $param);
+			} elseif (is_string($param)) {
+				$param = $pdo->quote($param);
+			} else {
+				// todo: co v tomto případě?
+			}
+			$sql = preg_replace('~\?~', $param, $sql, 1);
+		}
+		$pdo = null; // let's GC destroy that
+		return $sql;
+	}
+
+	function __toString()
+	{
+		return $this->interpolate();
 	}
 }
