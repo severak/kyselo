a8dd27f6785ba0c65a3080716ae532e0ccd14bd7
Severak <strajt9@seznam.cz>
2018-04-12 15:39:18 +0200

    better tabs

--- lib/views/blog_header.php
+++ lib/views/blog_header.php
@@ -6,17 +6,21 @@
 // - $tab
 ?>
 <div class="pure-g">
-	<div class="pure-u-1-5 kyselo-big-profile"><img class="pure-img" src="<?php echo $blog['avatar_url']; ?>"/></div>
+	<div class="pure-u-1-5">
+		<div class="kyselo-big-profile">
+			<img class="pure-img" src="<?php echo $blog['avatar_url']; ?>"/>
+		</div>	
+	</div>
 	<div class="pure-u-4-5">
 		<h1><?php echo $blog['title']; ?></h1>
 		<small><?php echo $blog['about']; ?></small>
-		<div class="kyselo-tabs">
-			<a href="/<?php echo $blog['name']; ?>" class="pure-button <?php echo $tab=='blog' ? 'pure-button-active': ''; ?>"><i class="fa fa-user"></i> <?php echo $blog['name']; ?></a>
-			<a href="/<?php echo $blog['name']; ?>/friends" class="pure-button <?php echo $tab=='friends' ? 'pure-button-active': ''; ?>"><i class="fa fa-users"></i> friends</a>
+		<ul class="kyselo-tabs">
+			<li <?=($tab=='blog' ? 'class="active"' : ''); ?> ><a href="/<?php echo $blog['name']; ?>"><i class="fa fa-user"></i> <?php echo $blog['name']; ?></a></li>
+			<li <?=($tab=='friends' ? 'class="active"' : ''); ?>><a href="/<?php echo $blog['name']; ?>/friends"><i class="fa fa-users"></i> friends</a></li>
 			<?php if (!empty($user) && $blog['id']==$user['blog_id']) { ?>
-			<a href="/act/settings/<?php echo $blog['name']; ?>" class="pure-button <?php echo $tab=='settings' ? 'pure-button-active': ''; ?>"><i class="fa fa-cog"></i> settings</a>
+			<li <?=($tab=='settings' ? 'class="active"' : ''); ?>><a href="/act/settings/<?php echo $blog['name']; ?>"><i class="fa fa-cog"></i> settings</a></li>
 			<?php } ?>
-		</div>
+		</ul>
 	</div>
 </div>
-<hr/>
+<hr class="kyselo-tabs-base">
