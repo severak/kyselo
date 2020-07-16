3a9e4674126e01a56c8d4d1b7d31663ce523025e
severak <strajt@dalten.cz>
2020-07-17 00:07:24 +0200

    Members can change group settings.

--- lib/views/blog_header.php
+++ lib/views/blog_header.php
@@ -17,7 +17,7 @@
 		<ul class="kyselo-tabs">
 			<li <?=($tab=='blog' ? 'class="active"' : ''); ?> ><a href="/<?php echo $blog['name']; ?>"><i class="fa fa-user"></i> <?php echo $blog['name']; ?></a></li>
 			<li <?=($tab=='friends' ? 'class="active"' : ''); ?>><a href="/<?php echo $blog['name']; ?>/friends"><i class="fa fa-users"></i> friends</a></li>
-			<?php if (!empty($user) && $blog['id']==$user['blog_id']) { ?>
+			<?php if (( !empty($user) && $blog['id']==$user['blog_id'] ) || (!empty($_SESSION['user']['groups'][$blog['id']]))) { ?>
 			<li <?=($tab=='settings' ? 'class="active"' : ''); ?>><a href="/act/settings/<?php echo $blog['name']; ?>"><i class="fa fa-cog"></i> settings</a></li>
 			<?php } ?>
 		</ul>
