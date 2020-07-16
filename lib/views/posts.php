9382a1dd2852c253ce6b97967f3de6e660381e3e
severak <strajt@dalten.cz>
2020-07-16 23:01:32 +0200

    Buttons to posting to group.

--- lib/views/posts.php
+++ lib/views/posts.php
@@ -176,9 +176,11 @@ if (empty($user)) {
 		echo '<a href="/act/post?as='.$user['name'].'&type=3" class="pure-button button-large"><i class="fa fa-paragraph"></i><span class="kyselo-hidden"> new post</span></a>';
 		echo '<a href="/act/post?as='.$user['name'].'&type=4" class="pure-button button-large"><i class="fa fa-camera"></i><span class="kyselo-hidden"> new post</span></a>';
 		echo '<a href="/act/post?as='.$user['name'].'&type=5" class="pure-button button-large"><i class="fa fa-youtube-play"></i><span class="kyselo-hidden"> new post</span></a>';
+		/*
 		echo '<a href="/act/post?as='.$user['name'].'&type=6" class="pure-button button-large"><i class="fa fa-file"></i><span class="kyselo-hidden"> new post</span></a>';
 		echo '<a href="/act/post?as='.$user['name'].'&type=7" class="pure-button button-large"><i class="fa fa-star"></i><span class="kyselo-hidden"> new post</span></a>';
 		echo '<a href="/act/post?as='.$user['name'].'&type=8" class="pure-button button-large"><i class="fa fa-calendar"></i><span class="kyselo-hidden"> new post</span></a>';
+		*/
 		echo '</span>';
 	}
 	// group:
@@ -189,6 +191,18 @@ if (empty($user)) {
 		$membership = Flight::db()->from('memberships')->where('member_id', $user['blog_id'])->where('blog_id', $blog['id'])->one();
 		// member
 		echo '<a href="/act/member?who='.$blog['name'].'" class="pure-button button-large kyselo-switch '.($membership ? 'on' : 'off').' '.(!empty($friendship['is_admin']) ? 'is-admin' : '').'"><i class="fa fa-user-plus"></i><span class="kyselo-hidden"> member</span></a>';	
+	
+		if ($membership) {
+            echo '<a href="#post_types" id="new_post" class="pure-button button-large"><i class="fa fa-pencil"></i><span class="kyselo-hidden"> new post</span></a>';
+            echo '<span id="post_types">';
+            echo '<a href="/act/post?as='.$blog['name'].'&type=1" class="pure-button button-large"><i class="fa fa-book"></i><span class="kyselo-hidden"> new post</span></a>';
+            echo '<a href="/act/post?as='.$blog['name'].'&type=2" class="pure-button button-large"><i class="fa fa-link"></i><span class="kyselo-hidden"> new post</span></a>';
+            echo '<a href="/act/post?as='.$blog['name'].'&type=3" class="pure-button button-large"><i class="fa fa-paragraph"></i><span class="kyselo-hidden"> new post</span></a>';
+            echo '<a href="/act/post?as='.$blog['name'].'&type=4" class="pure-button button-large"><i class="fa fa-camera"></i><span class="kyselo-hidden"> new post</span></a>';
+            echo '<a href="/act/post?as='.$blog['name'].'&type=5" class="pure-button button-large"><i class="fa fa-youtube-play"></i><span class="kyselo-hidden"> new post</span></a>';
+            echo '</span>';
+        }
+	
 	}
 	// other blog:
 	if (!empty($blog) && $blog['name']!=$user['name'] && !$blog['is_group']) {
