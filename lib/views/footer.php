9c2e5aa13c417f3574925aeb8273cbb3a5d2fa23
severak <strajt@dalten.cz>
2020-07-18 21:05:43 +0200

    fixed footer

--- lib/views/footer.php
+++ lib/views/footer.php
@@ -1,3 +1,9 @@
+
+</div>
 <script src="/st/js/kyselo.js?v=2020-07-17"></script>
+<?php if (Flight::config('footer_javascript')) {
+    echo Flight::config('footer_javascript');
+
+}?>
 </body>
 </html>
