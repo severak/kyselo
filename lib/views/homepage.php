d8cf12dd8f84cf931e00023ff5cb2fa87f4fe2bb
severak <strajt@dalten.cz>
2020-07-23 15:21:26 +0200

    fixed links to archive.org.

--- lib/views/homepage.php
+++ lib/views/homepage.php
@@ -23,7 +23,7 @@
 <div class="pure-g">
     <div class="pure-u-1-2">
         <h2>History</h2>
-        <p>Back then in 2011 I discovered Soup.io.</p>
+        <p>Back then in 2011 I discovered <a href="https://web.archive.org/web/20200430192929/https://www.soup.io/">Soup.io</a>.</p>
         <p>It quickly became my favorite source of memes, polish jokes, pis of cute girls & cats. I survived there a lot of hard times and <em title="AKA 2015">the year that never happened</em>.</p>
     </div>
     <div class="pure-u-1-2">
@@ -31,8 +31,8 @@
     </div>
     <div class="pure-u-1-1">
         <p>I started to keeping my diary there which turned to be bad idea - Soup was all the 503s and no stability. So I decided to create my own clone which I did.</p>
-        <p>Now, in 2020, as part of ongoing apocalypse, <a href="https://kitchen.soup.io/post/696483222/The-sadest-news-in-the-soup-history">Soup.io is closing it's doors</a>. </p>
-        <p>So I decided to <a href="https://kyselo-beta.svita.cz/severak/post/7">open my clone</a> for general public.</p>
+        <p>Now, in 2020, as part of ongoing apocalypse, <a href="https://web.archive.org/web/20200712165208/https://kitchen.soup.io/post/696483222/The-sadest-news-in-the-soup-history">Soup.io is closing it's doors</a>. </p>
+        <p>So I decided to <a href="https://kyselo.eu/severak/post/7">open my clone</a> for general public.</p>
     </div>
 </div>
 <hr>
