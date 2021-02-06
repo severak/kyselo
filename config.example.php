d6ccf1a5d4d6afb393142073e9b1b9c85f119ad9
severak <strajt@dalten.cz>
2021-02-06 11:56:51 +0100

    Documented all the config directives.

--- config.example.php
+++ config.example.php
@@ -1,14 +1,15 @@
-<?php
-// path to database
-$config['database'] = 'data/kyselo.sqlite';
-// adminer password
-$config['adminer_password'] = md5('jelen');
-// site base url
-$config['site_url'] = 'http://localhost';
-// site name
-$config['site_name'] = '';
-// secret random seed
-$config['secret'] = 'jhjhlkjhkhuiuyuiy';
-// invitation code
-$config['invitation_code'] = ['xyzzy'];
-return $config;
+<?php return array (
+    'site_name' => 'LOKALHOST',
+    'site_url' => 'http://192.168.0.156',
+    'secret' => 'S0m3th1ng r4nd0m',
+    'adminer_password' => md5('admin'),
+    'database' => 'db/kyselo.sqlite',
+    'invitation_code' => ['xyzzy'],
+    'show_debug'=>true,
+    'hosted_by' => 'underpant gnomes',
+    'hosted_by_url' => 'http://example.org',
+    'tos_post'=>1,
+    'footer_javascript'=>'<script>console.log("external stats  here")</script>',
+    'souper_endpoint_key'=>'some_random_string',
+    // 'maintance_mode'=>true,
+);
