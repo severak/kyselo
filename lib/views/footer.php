d796eb086d95ab3ce4ede11348df95f08a98d067
severak <strajt@dalten.cz>
2021-08-10 23:07:09 +0200

    Bump JS version.

--- lib/views/footer.php
+++ lib/views/footer.php
@@ -2,16 +2,16 @@
     <p>Kyselo  - <a href="https://bitbucket.org/severak/kyselo">opensource software</a> by <a href="http://tilde.town/~severak/">Severák</a>.
     <?php if (Flight::config('hosted_by')) { ?>
     Hosted by <?php
-    if (Flight::config('hosted_by_url')) echo '<a href="' . Flight::config('hosted_by_url') . '">'; 
-    echo Flight::config('hosted_by'); 
-    if (Flight::config('hosted_by_url')) echo '</a>'; 
+    if (Flight::config('hosted_by_url')) echo '<a href="' . Flight::config('hosted_by_url') . '">';
+    echo Flight::config('hosted_by');
+    if (Flight::config('hosted_by_url')) echo '</a>';
     ?>.
     <?php } // if ?>
     </p>
     </footer>
 </div>
 
-<script src="/st/js/kyselo.js?v=2020-01-25"></script>
+<script src="/st/js/kyselo.js?v=2021-08-10"></script>
 <?php if (Flight::config('footer_javascript')) {
     echo Flight::config('footer_javascript');
 
