61fa8a51025b3fe643b5c601b835a22dbcb91e20
severak <strajt@dalten.cz>
2021-12-22 23:53:36 +0100

    WIP - restoring backup.

--- lib/routes/backup.php
+++ lib/routes/backup.php
@@ -31,3 +31,32 @@ Flight::route('/act/backup', function (){
     } while ($filter->moveToNextPage());
     die;
 });
+
+Flight::route('/act/restore', function() {
+    $user = Flight::user();
+    if (!($user && $user['id']==1)) Flight::forbidden();
+
+    $rows = Flight::rows();
+
+    $blogs = $rows->more('blogs', ['is_visible'=>1, 'is_group'=>0], ['name'=>'ASC'], 999);
+    $blogSelect = array_column($blogs, 'name', 'id');
+
+    $form = new severak\forms\form(['method'=>'post']);
+    $form->field('upload', ['type'=>'file', 'label'=>'Backup file']);
+    $form->field('url_prefix', ['label'=>'Image URL prefix', 'required'=>true]);
+    $form->field('blog', ['type'=>'select', 'options'=>$blogSelect]);
+    $form->field('check', ['type'=>'submit', 'label'=>'Check it!']);
+
+    // TODO - upload backupu
+    // TODO - čtení řádku po řádce
+    // TODO - krok 1: nalezení fotky a pokus o její stažení
+    // TODO - krok 2: uložení do DB
+
+    // TODO - kyselo_upload_tmp
+    // TODO - kyselo_mirror_image
+
+
+    Flight::render('header', ['title' => 'restore backup' ]);
+    Flight::render('form', ['h2'=>'restore backup', 'form' => $form]);
+    Flight::render('footer', []);
+});
